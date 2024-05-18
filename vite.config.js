5da7694dfc5eb0526afc2495ee53acb598bafab8
Gogul11 <gogulsmart11@gmail.com>
2024-05-18 12:27:46 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/Frontendmentor-challenge-13/'
+  base : 'Frontendmentor-challenge-13'
 })
